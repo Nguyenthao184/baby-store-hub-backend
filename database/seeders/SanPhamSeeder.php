aa3b14c97cb4ca1cafb49c998d9587551cc8a663
Thảo Nguyên <nthaonguyen184308@gmail.com>
2025-09-08 14:37:50 +0700

    feat: update code

--- database/seeders/SanPhamSeeder.php
+++ database/seeders/SanPhamSeeder.php
@@ -22,48 +22,101 @@ class SanPhamSeeder extends Seeder
                 ['Sữa Friso Gold 2', 'sua-friso-gold-2.png'],
                 ['Sữa Nan Optipro 3', 'sua-nan-optipro-3.png'],
                 ['Sữa Abbott Grow 4', 'sua-abbott-grow-4.png'],
+                ['Sữa Abbott Grow', 'sua-abbott-grow.png'],
+                ['Sữa Similac Neosure', 'sua-similac-neosure.png'],
+                ['Sữa Similac Neosure ', 'sua-similac-total-comfort.png'],
+                ['Sữa Enfamil Enspire Infant Formula', 'sua-enfamil-enspire.png'],
+                ['Sữa Enfamil NeuroPro Infant Formula ', 'sua-enfamil-neuropro.png'],
+                ['Sữa Abbott Grow 1 ', 'sua-abbott-grow-1.jpg'],
+
             ],
             'Bỉm, tã' => [
                 ['Bỉm Pampers Newborn', 'bim-pampers-newborn.png'],
                 ['Bỉm Huggies Dry', 'bim-huggies-dry.png'],
                 ['Bỉm Bobby Extra Soft', 'bim-bobby-extra-soft.png'],
                 ['Tã Merries Nhật Bản', 'ta-merries-nhat-ban.png'],
+                ['Tã quần Huggies Skincare', 'ta-quan-huggies.png'],
+                ['Bỉm tã quần Bobby', 'ta-quan-bobby.png'],
+                ['Bỉm tã dán Moony', 'ta-dan-moony.png'],
+                ['Bỉm tã quần thiên nhiên Molfix Jumbo', 'bim-ta-quan-thien-nhien.png'],
+                ['Bỉm tã dán Huggies Platinum Nature Made', 'bim-ta-dan-huggies-platinum.png'],
+                ['Bỉm tã quần Moony bé trai', 'ta-quan-moony-be-trai.png'],
+
             ],
             'Thực phẩm - Đồ uống' => [
                 ['Bột ăn dặm Nestle', 'bot-an-dam-nestle.png'],
                 ['Cháo tươi SG Food', 'chao-tuoi-sg-food.png'],
                 ['Nước trái cây Pigeon', 'nuoc-trai-cay-pigeon.png'],
                 ['Súp dinh dưỡng Heinz', 'sup-dinh-duong-heinz.png'],
+                ['Xúc Xích Tiệt Trùng Goldkids Cua & Phô Mai', 'xuc-xich-cua-pho-mai.png'],
+                ['Thực phẩm bổ sung phô mai Con Bò Cười vuông Belcube vị truyền thống', 'pho-mai-con-bo-cuoi-vuong-le.png'],
+                ['Dinh dưỡng 100% trái cây nghiền hữu cơ HiPPiS Organic (Kiwi, Lê, Chuối)', 'trai-cay-nghien-huu-co.png'],
+                ['Phô Mai hoa quả Kids Mix Vị Dâu Mâm Xôi 50g - Lốc 4', 'pho-mai-hoa-qua-kids-mix-vi-dau-mam-xoi.jpg'],
+                ['Váng sữa Hoff - Vani (Lốc 4 hủ)', 'vang-sua-hff-vani.jpg'],
+                ['Mì ăn dặm Hakubaku - Mì Somen Baby (từ 5 tháng tuổi)', 'qt-morinaga-mi-an-dam.jpg'],
+
             ],
             'Sức khoẻ & Vitamin' => [
                 ['Vitamin C ChildLife', 'vitamin-c-childlife.png'],
                 ['Siro tăng đề kháng PediaSure', 'siro-tang-de-khang-pediasure.png'],
                 ['Men vi sinh BioGaia', 'men-vi-sinh-biogaia.png'],
                 ['DHA Bio Island', 'dha-bio-island.png'],
+                ['Thực phẩm bổ sung thạch hồng sâm trẻ em NFood', 'thuc-pham-bo-sung-thach-hong-sam-tre-em-nfood.png'],
+                ['Thực phẩm bảo vệ sức khỏe Fitobimbi Sonno', 'vitamin-fitobimbi-sonno.jpg'],
+                ['Men vi sinh Synteract Baby Drops Oil ', 'men-vi-sinh-synteract-baby-drops-oil.png'],
+                ['Thực phẩm bảo vệ sức khỏe Fitobimbi Sonno', 'fitobimbi-sonno.jpg'],
+                ['Ferrolip baby', 'ferrolip-baby.png'],
+                ['Thực phẩm bổ sung thạch Calci trẻ em NFood hương đào', 'thuc-pham-bo-sung-thach-calci-tre-em-nfood-huong-dao.png'],
             ],
             'Chăm sóc - Mỹ phẩm' => [
                 ['Kem chống hăm Bepanthen', 'kem-chong-ham-bepanthen.png'],
                 ['Sữa tắm gội Arau Baby', 'sua-tam-goi-arau-baby.png'],
                 ['Dầu dưỡng Bio Oil', 'dau-duong-bio-oil.png'],
                 ['Kem dưỡng ẩm Cetaphil', 'kem-duong-am-cetaphil.png'],
+                ['Sữa tắm gội toàn thân Johnson Baby 200ml', 'sua-tam-goi-toan-than-johnson-baby.jpg'],
+                ['Sữa tắm gội Lactacyd Baby Extra Milky 500ml ', 'sua-tam-goi-ngua-rom-say-cho-be-lactacyd-milky.jpg'],
+                ['Tắm gội dịu nhẹ Pigeon Jojoba 200ml (không paraben)', 'tam-goi-diu-nhe-pigeon-jojoba-200ml-khong-paraben.png'],
+                ['Phấn phủ làm dịu da Goongbe Pri-mmune 25g', 'phan-phu-lam-diu-da-goongbe-pri-mmune.png'],
+                ['Kem làm dịu hăm tã Goongbe Pri-mmune 80ml', 'kem-lam-diu-ham-ta-goongbe-pri-mmune.png'],
+                ['Kem chống hăm, chống nẻ trẻ em Crevil 125ml', 'kem-chong-ham-chong-ne-tre-em-crevil.png'],
+
             ],
             'Đồ dùng - Gia dụng' => [
                 ['Bình sữa Comotomo', 'binh-sua-comotomo.png'],
                 ['Máy tiệt trùng bình sữa', 'may-tiet-trung-binh-sua.png'],
                 ['Ghế ăn dặm Mastela', 'ghe-an-dam-mastela.png'],
                 ['Nhiệt kế điện tử Omron', 'nhiet-ke-dien-tu-omron.png'],
+                ['Ty ngậm Mam start 0-2m (girls)', 'ty-ngam-mam-start-0-2m-girls.jpg'],
+                ['Nhiệt kế hồng ngoại đo trán Microlife FR1MF1', 'nhiet-ke-hong-ngoai-do-tran-microlife-fr1mf.png'],
+                ['Máy hút sữa điện đơn Spectra M1', 'may-hut-sua-dien-don-spectra-m1.jpg'],
+                ['Khăn tắm cotton ConCung Good BM9T màu trắng', 'khan-tam-cotton-concung-good-bm9t-mau-trang.jpg'],
+                ['Xe đẩy hai chiều cao cấp Cool Baby màu xám', 'xe-day-2-chieu-cao-cap-cool-baby-c008h-xam.jpg'],  
+                ['Bình tập uống chống tràn MAM Starter Cup 150ml màu hồng', 'binh-tap-uong-mam-starter-cup-150ml-girls.jpg'],
             ],
+
             'Thời trang và phụ kiện' => [
                 ['Bộ quần áo Carter', 'bo-quan-ao-carter.png'],
                 ['Mũ len cho bé', 'mu-len-cho-be.png'],
                 ['Vớ sơ sinh', 'vo-so-sinh.png'],
                 ['Yếm ăn chống thấm', 'yem-an-chong-tham.png'],
+                ['Hộp vuông thun lớn cột tóc cho bé Animo A2204_MN024', 'hop-vuong-thun-lon-cot-toc-cho-be-animo-nhieu-mau.jpg'],
+                ['Đầm vải bé gái Animo TX822003 (6M-6Y,Hồng)', 'dam-vai-be-gai-animo-tx822003-6-9m-hong.jpg'],
+                ['Bodysuit đùi Animo Easy KV0924067 (0-12M,Nhiều màu)', 'bodysuit-dui-animo-easy-nhieu-mau.jpg'],
+                ['Bodysuit tính năng tam giác, vải modal BST Thiên Nga Animo BMC822080 (0-12M,Vàng)', 'bodysuit-tinh-nang-tam-giac-vai-modal-bst-thien-nga-animo-bmc822080-0-12m-vang.jpg'],
+                ['Bodysuit tính năng tam giác, vải lưới Animo I0322026 (0-12M,Beige,Giao mẫu ngẫu nhiên)', 'bodysuit-tinh-nang-tam-giac-vai-luoi-animo-beige.png'],
             ],
+
             'Đồ chơi, học tập' => [
                 ['Đồ chơi xúc xắc', 'do-choi-xuc-xac.png'],
                 ['Xe tập đi cho bé', 'xe-tap-di-cho-be.png'],
                 ['Bảng chữ cái nam châm', 'bang-chu-cai-nam-cham.png'],
                 ['Đồ chơi xếp hình Lego', 'do-choi-xep-hinh-lego.png'],
+                ['Vali kéo đi biển hình con vịt 8pcs', 'vali-keo-di-bien-hinh-con-vit.jpg'],
+                ['Đồ chơi bé trổ tài đầu bếp Polesie', 'do-choi-be-tro-tai-dau-bep-polesie.jpg'],
+                ['Lưới thảy vòng vịt bánh xe HT078 (TM)', 'luoi-thay-vong-vit-banh-xe.jpg'],
+                ['Xe Tập Đi Cho Bé Autoru AUBW02 (màu ghế ngồi ngẫu nhiên)', 'xe-tap-di-cho-be-autoru-aubw02.jpg'],
+                
+              
             ],
         ];
 
@@ -89,7 +142,7 @@ class SanPhamSeeder extends Seeder
                 $quantities[$randIndex]++;
             }
 
-             // foreach ($products as $i => $product) {
+            // foreach ($products as $i => $product) {
             //     $maSanPham = 'SP' . str_pad($counter, 4, '0', STR_PAD_LEFT);
 
             //     $sanPhamData[] = [
